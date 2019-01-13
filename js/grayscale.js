93783a29b7a839907c5b88e51e354d58c3e16ae6
StanislavJochman <s.jochman@gmail.com>
2019-01-13 11:43:21 +0100

    commit

--- js/grayscale.js
+++ js/grayscale.js
@@ -52,3 +52,42 @@ function send_email(){
      window.open("https://script.google.com/macros/s/AKfycbxN9Wf8d5qB-mFXKvrGMRkLz8sB1HggV-CT78KrPjjCI8qB0Il7/exec?&Data1=" + email_passed,'_blank', 'toolbar=no,status=no,menubar=no,scrollbars=no,resizable=no,left=10000, top=10000, width=0.1, height=0.1, visible=none', '');
   }
 }
+
+
+//Modals
+
+// Get the modal
+var modal = document.getElementById('SJ_Modal');
+var modal2 = document.getElementById('VN_Modal');
+// Get the button that opens the modal
+var btn = document.getElementById("StanislavJochman_img");
+var btn2 = document.getElementById("VeronikaNemjova_img");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("SJ_close")[0];
+var span2 = document.getElementsByClassName("VN_close")[0];
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
